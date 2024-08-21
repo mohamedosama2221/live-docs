3322e73da933ab903f47878b12b1e26cad31cf06
Mohamed Osama <eng.muhamed.osama@gmail.com>
2024-08-21 13:38:13 +0300

    [ADD] server action for creating a room

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,3 +2,20 @@ declare type HeaderProps = {
   children: React.ReactNode;
   className?: string;
 };
+
+declare type AccessType = ["room:write"] | ["room:read", "room:presence:write"];
+
+declare type RoomAccesses = Record<string, AccessType>;
+
+declare type UserType = "creator" | "editor" | "viewer";
+
+declare type RoomMetadata = {
+  creatorId: string;
+  email: string;
+  title: string;
+};
+
+declare type CreateDocumentParams = {
+  userId: string;
+  email: string;
+};
