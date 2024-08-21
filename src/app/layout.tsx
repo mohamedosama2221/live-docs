700d61cc32b26d8bc44f39ca594a617aa3de47e4
Mohamed Osama <eng.muhamed.osama@gmail.com>
2024-08-21 11:08:40 +0300

    [ADD] Header component -WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
+import Header from "@/components/Header";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -22,6 +23,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
           fontSans.variable
         )}
       >
+        <nav>
+          <Header className="sticky left-0 top-0">
+            <div className="flex items-center gap-2 lg:gap-4"></div>
+          </Header>
+        </nav>
         <main>{children}</main>
       </body>
     </html>
