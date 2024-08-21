55a8baee38e53c1c187e880ae0b3c7f83b1ee634
Mohamed Osama <eng.muhamed.osama@gmail.com>
2024-08-21 11:29:32 +0300

    [ADD] Authentication system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,14 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 import Header from "@/components/Header";
-
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -15,21 +22,34 @@ interface RootLayoutProps {
 }
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <nav>
-          <Header className="sticky left-0 top-0">
-            <div className="flex items-center gap-2 lg:gap-4"></div>
-          </Header>
-        </nav>
-        <main>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF" },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <nav>
+            <Header className="sticky left-0 top-0">
+              <div className="flex items-center gap-2 lg:gap-4"></div>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </Header>
+          </nav>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
